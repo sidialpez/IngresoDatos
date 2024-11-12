cc69131c608ab811c4b035cad2a9f9c5ef385099
Sidia Lopez <sidialpez@gmail.com>
2024-11-11 21:08:02 -0600

    quite el interceptor

--- src/main/java/com/sesion6/clase/Interceptor/MyInterceptor.java
+++ src/main/java/com/sesion6/clase/Interceptor/MyInterceptor.java
@@ -10,13 +10,13 @@ import java.util.List;
 
 public class MyInterceptor implements HandlerInterceptor {
     //este nos da el formato de la hora, la primer inicia y la otra bloquea
-    private final LocalTime starTime = LocalTime.of(10,0);
+    //private final LocalTime starTime = LocalTime.of(10,0);
 
-    private final LocalTime endTime = LocalTime.of(17,0);
+    /*private final LocalTime endTime = LocalTime.of(17,0);
     private final List<String> ipsPermitidas = Arrays.asList(
         "0:0:0:0:0:0:0:1",
         "127.0.0.1"
-    );
+    );*/
 
 
     /*@Override
@@ -37,7 +37,7 @@ public class MyInterceptor implements HandlerInterceptor {
         return true;
     }*/
 
-    @Override
+    /*@Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
 
        String clientIp = request.getRemoteAddr();
@@ -53,9 +53,9 @@ public class MyInterceptor implements HandlerInterceptor {
        }
         System.out.println("Acceso Permitido");
        return true;
-    }
+    }*/
 
-    @Override
+   /*@Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
                            Object handler, org.springframework.web.servlet.ModelAndView modelAndView)
             throws Exception{
@@ -66,14 +66,14 @@ public class MyInterceptor implements HandlerInterceptor {
 
         System.out.println("Solicitud manejando: "+httpMethod+" "+requestUrl+" "+clienteIp);
 
-    }
+    }*/
 
-    @Override
+   /* @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
                                 Object handler, Exception exception) throws Exception{
         System.out.println("After completion invocado");
         //simular alguna tarea para guardar en bitacora
 
-    }
+    }*/
 
 }
